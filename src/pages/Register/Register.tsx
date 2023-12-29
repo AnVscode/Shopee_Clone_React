dbb5c04413efab92bcc7718d537bd7eb811c77bf
AnVscode <ledaian22@gmail.com>
2023-12-30 03:12:15 +0700

    feat: validate register form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,44 +1,63 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+import { rules } from 'src/utils/rules'
+
+interface FormData {
+  email: string
+  password: string
+  confirm_password: string
+}
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
     <div className='bg-orange'>
       <div className='max-w-7xl mx-auto px-4'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-20 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng ký</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Email'
+                  {...register('email', rules.email)}
                 />
                 {/* show err email */}
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
               </div>
-              <div className='mt-3'>
-                <input
-                  type='pasword'
-                  name='pasword'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Pasword'
-                />
-                {/* show err password */}
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
-              </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <input
                   type='password'
-                  name='confirm_pasword'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
-                  placeholder='Confirm Pasword'
+                  placeholder='Mật khẩu'
+                  {...register('password', rules.password)}
+                />
+                {/* show err password */}
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
+              </div>
+              <div className='mt-2'>
+                <input
+                  type='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+                  placeholder='Xác nhận Mật khẩu'
+                  {...register('confirm_password', rules.confirm_password)}
                 />
                 {/* show err confirm password */}
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.confirm_password?.message}</div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
                   Đăng ký
                 </button>
---
+++ src/utils/rules.ts
@@ -0,0 +1,61 @@
+import { RegisterOptions } from 'react-hook-form'
+
+type Rules = { [key in 'email' | 'password' | 'confirm_password']?: RegisterOptions }
+
+export const rules: Rules = {
+  email: {
+    required: {
+      value: true,
+      message: 'Email không được để trống'
+    },
+
+    pattern: {
+      value: /^\S+@\S+\.\S+$/,
+      message: 'Email không đúng định dạng'
+    },
+
+    minLength: {
+      value: 5,
+      message: 'Email phải có ít nhất 5 ký tự'
+    },
+
+    maxLength: {
+      value: 160,
+      message: 'Độ dài tối đa là 160 ký tự'
+    }
+  },
+
+  password: {
+    required: {
+      value: true,
+      message: 'Mật khẩu không được để trống'
+    },
+
+    minLength: {
+      value: 6,
+      message: 'Mật khẩu phải có ít nhất 6 ký tự'
+    },
+
+    maxLength: {
+      value: 160,
+      message: 'Mật khẩu tối đa 160 ký tự'
+    }
+  },
+
+  confirm_password: {
+    required: {
+      value: true,
+      message: 'Nhập lại mật khẩu không được để trống'
+    },
+
+    minLength: {
+      value: 6,
+      message: 'Nhập lại mật khẩu phải có ít nhất 6 ký tự'
+    },
+
+    maxLength: {
+      value: 160,
+      message: 'Nhập lại mật tối đa 160 ký tự'
+    }
+  }
+}
