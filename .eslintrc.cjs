e65d1231b144077bccca027569f625be331b34b3
An Le Dai <55825127+AnVscode@users.noreply.github.com>
2024-02-17 23:37:42 +0700

    feat: code ui and logic pagination (#19)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   extends: [
-    // Chúng ta sẽ dùng các rule mặc định từ các plugin mà chúng ta đã cài.
+    // Dùng các rule mặc định từ các plugin đã cài.
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,46 +1,73 @@
 import { useQuery } from '@tanstack/react-query'
+import { omitBy, isUndefined } from 'lodash'
 
 import AsideFilter from './components/AsideFilter'
 import Product from './components/Product'
 import SortProductList from './components/SortProductList'
 import useQueryParams from 'src/hooks/useQueryParams'
 import { productApi } from 'src/types/api/product.api'
+import Pagination from 'src/components/Pagination'
+import { ProductListConfig } from 'src/types/product.type'
+
+export type QueryConfig = {
+  [key in keyof ProductListConfig]: string
+}
 
 export default function ProductList() {
-  const queryParams = useQueryParams()
+  const queryParams: QueryConfig = useQueryParams()
+
+  const queryConfig: QueryConfig = omitBy(
+    {
+      page: queryParams.page || '1',
+      limit: queryParams.limit,
+      sort_by: queryParams.sort_by,
+      exclude: queryParams.exclude,
+      name: queryParams.name,
+      order: queryParams.order,
+      price_max: queryParams.price_max,
+      price_min: queryParams.price_min,
+      rating_filter: queryParams.rating_filter
+    },
+    isUndefined
+  )
 
   const { data } = useQuery({
-    queryKey: ['product', queryParams],
+    queryKey: ['product', queryConfig],
     queryFn: () => {
-      return productApi.getProduct(queryParams)
-    }
+      return productApi.getProduct(queryConfig as ProductListConfig)
+    },
+    keepPreviousData: true
   })
 
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
-        <div className='grid grid-cols-12 gap-6'>
-          {/* AsideFilter */}
-          <div className='col-span-3'>
-            <AsideFilter />
-          </div>
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            {/* AsideFilter */}
+            <div className='col-span-3'>
+              <AsideFilter />
+            </div>
 
-          {/* SortProductList */}
-          <div className='col-span-9'>
-            <SortProductList />
+            {/* SortProductList */}
+            <div className='col-span-9'>
+              <SortProductList />
 
-            {/* items */}
-            <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {/* product items */}
-              {data &&
-                data?.data.data.products.map((product) => (
+              {/* items */}
+              <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+                {/* product items */}
+                {data?.data.data.products.map((product) => (
                   <div key={product._id} className='col-span-1'>
                     <Product product={product} />
                   </div>
                 ))}
+              </div>
+
+              {/* Pagination */}
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
